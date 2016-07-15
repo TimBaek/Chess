96d0af672bc1df1211f589d2dc89b62a98672b61
hyun-gwon <hyun-gwon94@hotmail.com>
2016-07-15 19:44:14 -0400

    comment

--- piece.h
+++ piece.h
@@ -1,18 +1,35 @@
 #ifndef _PIECE_H
 #define _PIECE_H
+#include <memory>
+/*
+class Board {
+public:
+  void notify(int cr, int cc, int dr, int dc);
+};
+class TextDisplay {
+public:
+  void notify(int r, int c, int rr, int cc, char ch);
+};
 
+class GraphicDisplay {
+public:
+  void notify(int r, int c, int rr, int cc, char ch);
+};
+class Player;
+*/
 class Piece {
-  shared_ptr<Board> b_obs;
-  shared_ptr<TextDisplay> td_obs;
-  shared_ptr<GraphicDisplay> gd_obs;
-  shared_ptr<Player> player;
+ protected:
+  std::shared_ptr<Board> b_obs;
+  std::shared_ptr<TextDisplay> td_obs;
+  std::shared_ptr<GraphicDisplay> gd_obs;
+  std::shared_ptr<Player> player;
   int row;
   int col;
   char letter;
 
  public:
-  Piece(shared_ptr<Board> b, shared_ptr<TextDisplay> td, 
-    shared_ptr<GraphicDisplay> gd, shared_ptr<Player> p, int c, int r, char l);
+  Piece(std::shared_ptr<Board> b, std::shared_ptr<TextDisplay> td, 
+    std::shared_ptr<GraphicDisplay> gd, std::shared_ptr<Player> p, int c, int r, char l);
   virtual ~Piece();
   void notifyBoard(int dr, int dc);
   void notifyTextDisplay(int dr, int dc);
