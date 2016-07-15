7d16032faaa6f6af620c842c6c300bc0c1eb8fb3
hyun-gwon <hyun-gwon94@hotmail.com>
2016-07-15 19:06:37 -0400

    comment

--- piece.h
+++ piece.h
@@ -10,7 +10,6 @@ class Piece {
   int col;
   char letter;
 
-
  public:
   Piece(shared_ptr<Board> b, shared_ptr<TextDisplay> td, shared_ptr<GraphicDisplay gd, shared_ptr<Player> p, int c, int r, char l);
   virtual ~Piece();
@@ -21,13 +20,54 @@ class Piece {
   int getRow() const;
   int getCol() const;
   char getLetter() const;
-
-
-#endif
-
+};
 
 class King: public Piece {
-  bool evermoved = false;
+  bool moved = false;
+
  public:
-  
-}
+  King(bool b=false);
+  ~King();
+  void move(int row, int col);
+  bool everMoved();
+};
+
+class Queen: public Piece {
+ public:
+  Queen();
+  ~Queen();
+  void move(int row, int col);
+};
+
+class Rook: public Piece {
+  bool moved = false;
+
+ public:
+  Rook(bool b=false);
+  ~Rook();
+  void move(int row, int col);
+  bool everMoved();
+};
+
+class Knight: public Piece {
+ public:
+  Knight();
+  ~Knight();
+  void move(int row, int col);
+};
+
+class Bishop: public Piece {
+ public:
+  Bishop();
+  ~Bishop();
+  void move(int row, int col);
+};
+
+class Pawn: public Piece {
+ public:
+  Pawn();
+  ~Pawn();
+  void move(int row, int col);
+};
+
+#endif
