92ac5a0e4e04ee7f975b3e0f663963a8e8d0a329
hyun-gwon <hyun-gwon94@hotmail.com>
2016-07-16 20:01:08 -0400

    comment

--- piece.cc
+++ piece.cc
@@ -1,18 +1,19 @@
 #include "piece.h"
-#include "controller.h"
+#include "board.h"
 #include <memory>
+#include <string>
 using namespace std;
 
 // Piece 
-Piece::Piece(shared_ptr<Controller> ctrl, int r, int c, char l, string co):
-  ctrler{ctrl}, row{r}, col{c}, letter{l} colour{co} {
+Piece::Piece(shared_ptr<Board> bd, int r, int c, string co):
+  Board{bd}, row{r}, col{c}, colour{co} {
 }
 
 Piece::~Piece() { }
 
 
 void Piece::notifyController(int dr, int dc) {
-  ctrler->notify(*this, dr, dc);
+  board->notify(*this, dr, dc);
 }
 
 int Piece::getRow() const {
@@ -23,23 +24,21 @@ int Piece::getCol() const {
   return col;
 }
 
-char Piece::getLetter() const {
-  return letter;
-}
-
 string Piece::getColour() const {
   return colour;
 }
 
 // King
-King::King(shared_ptr<Controller> ctrl, int r, int c, char l, string co, bool b):
-  Piece{ctrl, r, c, l, co}, moved{b} {
+King::King(shared_ptr<Board> bd, int r, int c, string co, bool b):
+  Piece{bd, r, c, co}, moved{b} {
 }
 
 King::~King() { }
 
-void King::move(int row, int col) {
-// not finished
+void King::move(int dr, int dc) {
+  notifyBoard(dr, dc);
+  row = dr;
+  col = dc;
   moved = true;
 }
 
@@ -47,27 +46,46 @@ bool King::everMoved() {
   return moved;
 }
 
+char King::getLetter() {
+  if (colour == "white") {
+    return 'K';
+  } else {
+    return 'k';
+  }
+}
+
 // Queen
-Queen::Queen(shared_ptr<Controller> ctrl, int r, int c, char l, string co):
-  Piece{ctrl, r, c, l, co} {
+Queen::Queen(shared_ptr<Board> bd, int r, int c, string co):
+  Piece{bd, r, c, co} {
 }
 
 Queen::~Queen() { }
 
-void Queen::move(int row, int col) {
-// not finished
+void Queen::move(int dr, int dc) {
+  notifyBoard(dr, dc);
+  row = dr;
+  col = dc;
 }
 
+char Queen::getLetter() {
+  if (colour == "white") {
+   return 'Q';
+  } else {
+   return 'q';
+  }
+}
 
 // Rook
-Rook::Rook(shared_ptr<Controller> ctrl, int c, int r, char l, string co, bool b):
-  Piece{ctrl, r, c, l, co}, moved{b} {
+Rook::Rook(shared_ptr<Board> bd, int c, int r, string co, bool b):
+  Piece{bd, r, c, co}, moved{b} {
 }
 
 Rook::~Rook() { }
 
-void Rook::move(int row, int col) {
-// not finished
+void Rook::move(int dr, int dc) {
+  notifyBoard(dr, dc);
+  row = dr;
+  col = dc;
   moved = true;
 }
 
@@ -75,41 +93,79 @@ bool Rook::everMoved() {
   return moved;
 }
 
+char Rook::getLetter() {
+  if (colour == "white") {
+    return 'R';
+  } else {
+    return 'r';
+  }
+}
+
 // Knight
-Knight::Knight(shared_ptr<Controller> ctrl, int c, int r, char l, string co):
-  Piece{ctrl, r, c, l, co} {
+Knight::Knight(shared_ptr<Board> bd, int c, int r, string co):
+  Piece{bd, r, c, co} {
 }
 
 Knight::~Knight() { }
 
-void Knight::move(int row, int col) {
-// not finished
+void Knight::move(int dr, int dc) {
+  notifyBoard(dr, dc);
+  row = dr;
+  col = dc;
+}
+
+char Knight::getLetter() {
+  if (colour == "white") {
+    return 'N';
+  } else {
+   return 'n';
+  }
 }
 
 // Bishop 
-Bishop::Bishop(shared_ptr<Controller> ctrl, int c, int r, char l, string co):
-  Piece{ctrl, r, c, l, co} {
+Bishop::Bishop(shared_ptr<Board> bd, int c, int r, string co):
+  Piece{bd, r, c, co} {
 }
 
 Bishop::~Bishop() { }
 
-void Bishop::move(int row, int col) {
-// not finished
+void Bishop::move(int dr, int dc) {
+  notifyBoard(dr, dc);
+  row = dr;
+  col = dc;
+}
+
+char Bishop::getLetter() {
+  if (colour == "white") {
+    return 'B';
+  } else {
+    return 'b';
+  }
 }
 
 // Pawn
-Pawn::Pawn(shared_ptr<Controller> ctrl, int c, int r, char l, string co):
-  Piece{ctrl, r, c, l, co} {
+Pawn::Pawn(shared_ptr<Board> bd, int c, int r, string co, bool b):
+  Piece{bd, r, c, co} moved{b} {
 }
 
 Pawn::~Pawn() { }
 
-void Pawn::move(int row, int col) {
-
+void Pawn::move(int dr, int dc) {
+  notifyBoard(dr, dc);
+  row = dr;
+  col = dc;
+  moved = true;
 }
 
 bool Pawn::everMoved() {
   return moved;
 }
 
+char Pawn::getLetter() {
+  if (colour == "white") {
+    return 'P';
+  } else {
+    return 'p';
+  }
+}
 
--- piece.h
+++ piece.h
@@ -3,25 +3,23 @@
 #include <memory>
 #include <string>
 
-class Controller;
+class Board;
 
 class Piece {
  protected:
-  std::shared_ptr<Controller> ctrler;
+  std::shared_ptr<Board> board;
   int row;
   int col;
-  char letter;
   std::string colour;
 
  public:
-  Piece(std::shared_ptr<Controller> ctrl, 
-        int r, int c, char l, std::string co);
+  Piece(std::shared_ptr<Board> bd, int r, int c, std::string co);
   virtual ~Piece();
-  void notifyController(int dr, int dc);
-  virtual void move(int row, int col)=0;
+  void notifyBoard(int dr, int dc);
+  virtual void move(int dr, int dc)=0;
   int getRow() const;
   int getCol() const;
-  char getLetter() const;
+  virtual char getLetter()=0;
   std::string getColour() const;
 };
 
@@ -29,54 +27,57 @@ class King: public Piece {
   bool moved = false;
 
  public:
-  King(std::shared_ptr<Controller> ctrl, 
-       int r, int c, char l, std::string co, bool b=false);
+  King(std::shared_ptr<Board> bd, int r, int c, std::string co, bool b=false);
   ~King();
-  void move(int row, int col) override;
+  void move(int dr, int dc) override;
   bool everMoved();
+  char getLetter() override;
 };
 
 class Queen: public Piece {
  public:
-  Queen(std::shared_ptr<Controller> ctrl,
-        int r, int c, char l, string co);
+  Queen(std::shared_ptr<Board> bd, int r, int c, std::string co);
   ~Queen();
-  void move(int row, int col) override;
+  void move(int dr, int dc) override;
+  char getLetter() override;
 };
 
 class Rook: public Piece {
   bool moved = false;
 
  public:
-  Rook(std::shared_ptr<Controller> ctrl,
-       int r, int c, char l, std::string co, bool b=false);
+  Rook(std::shared_ptr<Board> bd, int r, int c, std::string co, bool b=false);
   ~Rook();
-  void move(int row, int col) override;
+  void move(int dr, int dc) override;
   bool everMoved();
+  char getLetter() override;
 };
 
 class Knight: public Piece {
  public:
-  Knight(std::shared_ptr<Controller> ctrl,
-	 int r, int c, char l, std::string co);
+  Knight(std::shared_ptr<Board> bd, int r, int c, std::string co);
   ~Knight();
-  void move(int row, int col) override;
+  void move(int dr, int dc) override;
+  char getLetter() override; 
 };
 
 class Bishop: public Piece {
  public:
-  Bishop(std::shared_ptr<Controller> ctrl,
-	 int r, int c, char l, std::string co);
+  Bishop(std::shared_ptr<Board> bd, int r, int c, std::string co);
   ~Bishop();
-  void move(int row, int col) override;
+  void move(int dr, int dc) override;
+  char getLetter() override;
 };
 
 class Pawn: public Piece {
+  bool moved = false;
+  
  public:
-  Pawn(std::shared_ptr<Controller> ctrl,
-       int r, int c, char l, std::string co);
+  Pawn(std::shared_ptr<Board> bd, int r, int c, std::string co, bool b=false);
   ~Pawn();
-  void move(int row, int col) override;
+  void move(int dr, int dc) override;
+  char getLetter() override;
+  bool everMoved();
 };
 
 #endif
