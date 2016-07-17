5fb908e6afd2bfc36e7936525d4fa040c64cc1b4
michellehy95 <michellehy95@gmail.com>
2016-07-17 15:37:46 -0400

    160717 3pm board

--- board.cc
+++ board.cc
@@ -1,98 +1,134 @@
 #include <iostream>
 #include <vector>
 #include "board.h"
+#include "bishop.h"
+#include "king.h"
+#include "knight.h"
+#include "pawn.h"
+#include "queen.h"
+#include "rook.h"
 using namespace std;
 
-Board::Board(TextDisplay *td, GraphicDisplay *gd): td{td}, gd{gd} {}
-Board::~Board() { 
-	delete td;
-	delete gd;
-}
+Board::Board(Controller *ctrl): ctrl{ctrl}, player1{""}, player2{""} {}
+Board::~Board() {}
 
-void Board::clearBoard(){ 
+void Board::clearBoard(){
 
 }
 
-void notify(int r, int c, int destr, int destc){
-
+void Board::notify(int r, int c, Piece &p){
+	updateState(r,c, (p.getRow()), (p.getCol()));
 }
 
-bool isThere(Piece &p, int r, int c){
-	return((p->getRow()==r) && (p->getCol()==c));
+bool Board::isThere(Piece &p, int r, int c){
+	return((p.getRow()==r) && (p.getCol()==c));
 }
 
-bool canMove(Piece &p, int r, int c);
+bool Board::canMove(Piece &p, int r, int c){
+	return true;
+}
 
-void updateState();
+void Board::updateState(int r, int c, int destr, int destc){
+	shared_ptr<Piece> tmp = currStates[r][c];
+	currStates[destr][destc] = tmp;
+	currStates[r][c] = nullptr;
+}
 
 
-void init(Player *player1, Player *player2){
-	//if(!td) td = 
+void Board::init(string p1, string p2){
+	player1 = p1;
+	player2 = p2;
 	for(int i=0; i<8; i++){
-		currStates.push_back(vector<Piece>(8));
-		for(int j=0; j<8; j++){
-			currStates[i][j] = (i+j)%2 ==0 ? ' ' : '_';
-		}
+		currStates.push_back(vector<shared_ptr<Piece>>(8));
+		for(int j=0; j<8; j++){ currStates[i][j] = nullptr; }
 	}
-	makePieces(player1, player2);	
-
+	player1 == "white" ? defaultSetup(player1, player2) : defaultSetup(player2, player1);
 }
 
-void defaultSetup(Player *player1, Player *player2){
+
+
+// Default setup:
+// colour1 is for the white player
+// colour2 is for the black player
+void Board::defaultSetup(string colour1, string colour2){
+	//shared_ptr<Board> b = make_shared<Board>(*this);
 	//pawns
 	for (int i=0; i<8; i++){
-		Pawn p1{this, player1, td, gd, 1, i, 'p'};
-		Pawn p2{this, player2, td, gd, 6, i, 'P'};
-		P1.push_back(p1);
-		P2.push_back(p2);
-		currStates[1][i] = 'p';
-		currStates[6][i] = 'P';
+		currStates[1][i] = make_shared<Pawn>(this,1,i, colour1);
+		currStates[6][i] = make_shared<Pawn>(this,6,i, colour2);
 	}
 	//rooks
 	for (int i=0; i<8; i+=7){
-		Rook r1{this, player1, td, gd, 0, i, 'r'};
-		Rook r2{this, player2, td, gd, 7, i, 'R'};
-		P1.push_back(r1);
-		P2.push_back(r2);
-		currStates[0][i] = 'r';
-		currStates[7][i] = 'R';
+		currStates[0][i] = make_shared<Rook>(this,0,i, colour1);
+		currStates[7][i] = make_shared<Rook>(this,7,i, colour2);
 	}
 	//knights
 	for (int i=1; i<8; i+=5){
-		Knight k1{this, player1, td, gd, 0, i, 'n'};
-		Knight k2{this, player2, td, gd, 7, i, 'N'};
-		P1.push_back(k1);
-		P2.push_back(k2);
-		currStates[0][i] = 'n';
-		currStates[7][i] = 'N';
+		currStates[0][i] = make_shared<Knight>(this,0,i, colour1);
+		currStates[7][i] = make_shared<Knight>(this,7,i, colour2);
 	}
 	//bishops
 	for (int i=2; i<8; i+=3){
-		Bishop b1{this, player1, td, gd, 0, i, 'b'};
-		Bishop b2{this, player2, td, gd, 7, i, 'B'};
-		P1.push_back(b1);
-		P2.push_back(b2);
-		currStates[0][i] = 'b';
-		currStates[7][i] = 'B';
+		currStates[0][i] = make_shared<Bishop>(this,0,i, colour1);
+		currStates[7][i] = make_shared<Bishop>(this,7,i, colour2);
 	}
 	//kings and queens
-	King k1{this, player1, td, gd, 0, 4, 'k'};
-	King k2{this, player2, td, gd, 7, 4, 'K'};
-	Queen q1{this, player1, td, gd, 0, 3, 'q'};
-	Queen q2{this, player2, td, gd, 7, 3, 'Q'};
-	P1.push_back(k1);
-	P1.push_back(q1);
-	P2.push_back(k2);
-	P2.push_back(q2);
-	currStates[0][4] = 'k';
-	currStates[7][4] = 'K';
-	currStates[0][3] = 'q';
-	currStates[7][3] = 'Q';
+	currStates[0][4] = make_shared<King>(this,0,4, colour1);
+	currStates[7][4] = make_shared<King>(this,7,4, colour2);
+	currStates[0][3] = make_shared<Queen>(this,0,3, colour1);
+	currStates[7][3] = make_shared<Queen>(this,7,3, colour2);
 }
 
 
 
-void setup();
 
-friend ostream &operator<<(std::ostream &out, const Board &b);
+void Board::setup(){
+	for(int i=0; i<8; i++){
+		currStates.push_back(vector<shared_ptr<Piece>>(8));
+		for(int j=0; j<8; j++){ currStates[i][j] = nullptr; }
+	}
+}
+
+void Board::setup_add(char letter, int r, int c){
+	setup_delete(r,c);
+	char tmp = (letter < 'Z') ? letter+'a'-'A' : letter;
+	string colour = (letter < 'Z') ? "white" : "black";
+	switch (tmp){
+		case 'k' : currStates[r][c] = make_shared<King>(this,r,c,colour);
+		case 'q' : currStates[r][c] = make_shared<King>(this,r,c,colour);
+		case 'b' : currStates[r][c] = make_shared<Bishop>(this,r,c,colour);
+		case 'p' : currStates[r][c] = make_shared<Pawn>(this,r,c,colour);
+		case 'r' : currStates[r][c] = make_shared<Rook>(this,r,c,colour);
+		case 'n' : currStates[r][c] = make_shared<Knight>(this,r,c,colour);
+	}
+}
+
+void Board::setup_delete(int r, int c){
+	if(currStates[r][c]==nullptr) return;
+	currStates[r][c] = nullptr;
+}
+
+int Board::setup_numKing(string colour){
+	int num=0;
+	char k = colour== "black" ? 'k' : 'K';
+	for(int i=0; i<8; i++){
+		for(int j=0; j<8; j++){
+			if((currStates[i][j]->getColour()==colour)&&
+				(currStates[i][j]->getLetter() == k))
+				num++;
+		}
+	}
+	return num;
+
+}
+
+
+shared_ptr<Piece> Board::checkState(int r, int c){
+	return currStates[r][c];
+}
+
+
+ostream &operator<<(std::ostream &out, const Board &b){
+	return out;
+}
 
