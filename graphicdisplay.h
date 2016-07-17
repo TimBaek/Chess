f509264145de9e08ef4b3eb95d6c7235d612ca3c
michellehy95 <michellehy95@gmail.com>
2016-07-17 17:15:24 -0400

    graphicdisplay 160717

--- graphicdisplay.h
+++ graphicdisplay.h
@@ -1,15 +1,18 @@
 #ifndef __GRAPHICDISPLAY_H__
 #define __GRAPHICDISPLAY_H__
+#include "view.h"
 #include <iostream>
 #include <vector>
 
-class GraphicDisplay {
+//class Xwindow;
+
+class GraphicDisplay: public View {
     
 	public:
 		GraphicDisplay();
 		~GraphicDisplay();
 		void notify(int ir, int ic, int dr, int dc);
-		friend std::ostream &operator<<(std::ostream &out, const TextDisplay &td);
+		friend std::ostream &operator<<(std::ostream &out, const GraphicDisplay &td);
 };
 
 #endif
