1a4e9689c2bd8eb219b77a039cbbeaeb61e74622
michellehy95 <michellehy95@gmail.com>
2016-07-17 17:13:30 -0400

    textdisplay 160717

--- textdisplay.cc
+++ textdisplay.cc
@@ -3,30 +3,26 @@
 using namespace std;
 
 TextDisplay::TextDisplay() {
-  for(int i=0; i<8; i++) {
+    for(int i=0; i<8; i++) {
         theDisplay.push_back(vector<char>(8));
         for(int j=0; j<8; j++) {
-        	if((i+j)%2 == 0) {
-        		theDisplay[i][j] = '_';
-        	} else {
-            	theDisplay[i][j] = ' ';
-            }
+        	theDisplay[i][j] = (i+j)%2 == 0 ? '_' : ' ';
         }
-  }
+    }
 }
-
 void TextDisplay::notify(int ir, int ic, int dr, int dc) {
+    theDisplay[dr][dc] = theDisplay[ir][ic];
+    theDisplay[ir][ic] = (ir+ic)%2 == 0 ? '_' : ' ';
 }
-
 // dtor
 TextDisplay::~TextDisplay() {}
 
 ostream &operator<<(ostream &out, const TextDisplay &td) {
-        for(int c=0; c < 8; ++c) {
-                for(int r=0; r < 8; ++r) {
-                        out << td.theDisplay[r][c];
-                }
-                out << endl;
+    for(int c=0; c < 8; ++c) {
+        for(int r=0; r < 8; ++r) {
+            out << td.theDisplay[r][c];
         }
-        return out;
+        out << endl;
+    }
+    return out;
 }
