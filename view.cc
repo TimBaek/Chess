a714bc27d9694b5cf31486024239b9b69df91ddb
jooyeonyoo <j29yoo@uwaterloo.ca>
2016-07-17 15:07:11 -0400

    comment

---
+++ view.cc
@@ -0,0 +1,12 @@
+#include "view.h"
+using namespace std;
+
+View::View() {
+}
+
+
+void View::notify(int ir, int ic, int dr, int dc) {
+}
+
+// dtor
+View::~View() {}
--- view.h
+++ view.h
@@ -1,7 +1,14 @@
 #ifndef __VIEW_H__
 #define __VIEW_H__
 #include <iostream>
+#include <vector>
 
-class View {};
+class View {
+
+   public:
+        View();
+        virtual ~View() = 0;
+        virtual void notify(int ir, int ic, int dr, int dc) = 0;
+};
 
 #endif
