43a548c8a1258bdca8726c06a39d46903527499b
michellehy95 <michellehy95@gmail.com>
2016-07-17 15:37:44 -0400

    160717 3pm board

--- board.h
+++ board.h
@@ -5,28 +5,39 @@
 #include "piece.h"
 #include "textdisplay.h"
 
-class Board {
-  std::vector<Piece> P1;
-  std::vector<Piece> P2;
-  std::vector<std::vector<char>> currStates;  // current state of the board
-  TextDisplay *td; // The text display.
-  GraphicDisplay *gd; // The graphic display.
+class TextDisplay;
+class GraphicDisplay;
+class Controller;
 
+class Board {
+  //std::vector<Piece> P1;
+  //std::vector<Piece> P2;
+  Controller *ctrl; // The Controller
+  std::string player1;
+  std::string player2;
+  std::vector<std::vector<std::shared_ptr<Piece>>> currStates;  // current state of the board
+  //std::vector<std::vector<std::std::vector<int>>> possibleMove; // possibleMoves
+ 
   void clearBoard();   // Frees the board.
-  void defaultSetup(Player *player1, Player *player2);
+  void defaultSetup(std::string colour1, std::string colour2);
+  void updateState(int r, int c, int destr, int destc);
 
  public:
-  Board(TextDisplay *td, GraphicDisplay *gd);
+  Board(Controller *ctrl);
   ~Board();
   
   //void notifyTd(TextDisplay *td);
-  void notify(int r, int c, int destr, int destc);
+  void notify(int r, int c, Piece &p);
 
   bool isThere(Piece &p, int r, int c);
-  bool canMove(Pice &p, int r, int c);
-  void updateState();
-  void init(Player *player1, Player *player2);
+  bool canMove(Piece &p, int r, int c);
+  //void init(std::shared_ptr<Player> player1, std::shared_ptr<Player> player2);
+  void init(std::string p1, std::string p2);
   void setup();
+  void setup_add(char letter, int r, int c);
+  void setup_delete(int r, int c);
+  int setup_numKing(std::string colour);
+  std::shared_ptr<Piece> checkState(int r, int c);
   friend std::ostream &operator<<(std::ostream &out, const Board &b);
 };
 
