a071d27d599956d18d392e329048be55fbcb900e
michellehy95 <michellehy95@gmail.com>
2016-07-21 18:33:04 -0400

    fixed minor errors

--- computer.cc
+++ computer.cc
@@ -5,7 +5,7 @@
 #include <sstream>
 using namespace std;
 
-Computer::Computer(string colour, int level): colour{colour}, stalemate{false}, checkmate{false}, check{false}, b{nullptr}, level{level}, currR{-1}, currC{-1}, destR{-1}, destC{-1} {}
+Computer::Computer(string colour, int level, Board *b): colour{colour}, stalemate{false}, checkmate{false}, check{false}, b{b}, level{level}, currR{-1}, currC{-1}, destR{-1}, destC{-1} {}
 
 Computer::~Computer() {}
 
@@ -20,13 +20,15 @@ bool Computer::isCheck() { return check; }
 
 void Computer::setBoard(Board *board){ b = board; }
 
-int Computer::getR(){ return currX; }
-int Computer::getC(){ return currY; }
-int Computer::getDestR(){ return destX; }
-int Computer::getDestC(){ return destY; }
+int Computer::getR(){ return currR; }
+int Computer::getC(){ return currC; }
+int Computer::getDestR(){ return destR; }
+int Computer::getDestC(){ return destC; }
 
 void Computer::nextMove(){
+cerr << "nextMove called" << endl;
 	if(level==1) randomMove();
+if(level==1) cerr << "level is 1" << endl;
 }
 
 void Computer::randomMove(){
@@ -46,12 +48,13 @@ void Computer::randomMove(){
 					if(!b->canMove(b->checkState(r,c), i, j, getColour())) continue;
 					v1.push_back(r);
 					v2.push_back(c);
-					v3.push_back(destr);
-					v4.push_back(destc);
+					v3.push_back(i);
+					v4.push_back(j);
 				}
 			}
 		}
 	}
+if(!(v1.size()==v2.size()==v3.size()==v4.size())) cerr << "***********" << endl;
 	srand (time(NULL));
 	int x = rand() % v1.size();
 	currR = v1[x];
