02364869175368fed32cf10b652e64c8b476bb0d
michellehy95 <michellehy95@gmail.com>
2016-07-21 22:06:23 -0400

    added computer level 2

--- computer.cc
+++ computer.cc
@@ -26,9 +26,9 @@ int Computer::getDestR(){ return destR; }
 int Computer::getDestC(){ return destC; }
 
 void Computer::nextMove(){
-cerr << "nextMove called" << endl;
+cout << "nextMove called with level " << level << endl;
 	if(level==1) randomMove();
-if(level==1) cerr << "level is 1" << endl;
+	else if(level==2){ avoidCapture(); }
 }
 
 void Computer::randomMove(){
@@ -36,13 +36,10 @@ void Computer::randomMove(){
 	vector<int> v2;
 	vector<int> v3;
 	vector<int> v4;
-	int index=0;
 	for(int r=0; r<8; r++){
 		for(int c=0; c<8; c++){
 			if(b->isEmpty(r,c)) continue;
 			if(b->checkState(r,c)->getColour() != getColour()) continue;
-			//char letter = b->checkState(r,c)->getLetter();
-			//letter = letter < 'Z' ? letter + 'a' - 'A' : letter;
 			for(int i=0; i<8; i++){
 				for(int j=0; j<8; j++){
 					if(!b->canMove(b->checkState(r,c), i, j, getColour())) continue;
@@ -54,7 +51,6 @@ void Computer::randomMove(){
 			}
 		}
 	}
-if(!(v1.size()==v2.size()==v3.size()==v4.size())) cerr << "***********" << endl;
 	srand (time(NULL));
 	int x = rand() % v1.size();
 	currR = v1[x];
@@ -62,3 +58,68 @@ if(!(v1.size()==v2.size()==v3.size()==v4.size())) cerr << "***********" << endl;
 	destR = v3[x];
 	destC = v4[x];
 }
+
+void Computer::avoidCapture(){
+	// priority of pieces to avoid captures for:
+	// pawn < knight < rook < bishop < queen < king
+	// each corresponds to 1-6 according to the priorityy
+	int r1,r2,c1,c2;
+	int currPriority=0;
+	string oppColour = getColour()=="black" ? "white" : "black";
+
+	for(int r=0; r<8; r++){
+		for(int c=0; c<8; c++){
+			if(b->isEmpty(r,c)) continue;
+			if(b->checkState(r,c)->getColour() == getColour()) continue;
+			for(int i=0; i<8; i++){
+				for(int j=0; j<8; j++){
+					if(b->checkState(i,j) == nullptr) continue;
+					if(!b->canMove(b->checkState(r,c), i, j, oppColour)) continue;
+					if(b->checkState(i,j)->getColour() != getColour()) continue;
+cout << "found a piece that's capturable: " << i << j << endl;
+					char c = b->checkState(i,j)->getLetter();
+					c = c < 'Z' ? c + 'a' - 'A' : c;
+cout << "and it's the piece " << c << endl;
+					int priority;
+					switch(c) {
+						case 'k': priority = 6; break;
+						case 'q': priority = 5; break;
+						case 'b': priority = 4; break;
+						case 'r': priority = 3; break;
+						case 'n': priority = 2; break;
+						case 'p': priority = 1;
+					}
+					if(priority<= currPriority) continue;
+					bool movable=false;
+					for(int x=0; x<8; x++){
+						if(movable) break;
+						for(int y=0; y<8; y++){
+							if(b->canMove(b->checkState(i,j),x,y, getColour())){
+								r2=x;
+								c2=y;
+								movable = true;
+								// stops the loop once it finds a move
+								// which might not necessarily be the best move.
+								break;
+							}
+						}
+					}
+					if(!movable) continue;
+					currPriority = priority;
+					r1=i;
+					c1=j;
+				}
+			}
+		}
+	}
+	if(currPriority==0){
+		randomMove();
+		return;
+	}
+	currR = r1;
+	currC = c1;
+	destR = r2;
+	destC = c2;
+}
+
+
