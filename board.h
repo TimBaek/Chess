6c571ed05068b4407b7336d5e8c4a458bfb476af
michellehy95 <michellehy95@gmail.com>
2016-07-24 17:45:46 -0400

    updated check, checkmate, stalemate

--- board.h
+++ board.h
@@ -59,6 +59,7 @@ class Board {
   std::shared_ptr<Piece> checkState(int r, int c);
   bool isPromo(int r, int c, int destr, int destc);
   bool willBeChecked(int r, int c, int dr, int dc, std::string colour);
+  bool isCheck(std::string colour);
   bool isCheckmate(std::string colour);
   bool isStalemate(std::string colour);
 };
